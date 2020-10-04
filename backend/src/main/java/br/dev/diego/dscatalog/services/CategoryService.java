e173a483fadb066193866d2decd2f708bd44ac9c
Diego Sousa <diegosous@gmail.com>
2020-10-03 23:21:10 -0300

    Find Category by id, exception handling

--- backend/src/main/java/br/dev/diego/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/dev/diego/dscatalog/services/CategoryService.java
@@ -1,14 +1,17 @@
 package br.dev.diego.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.dev.diego.dscatalog.dto.CategoryDTO;
 import br.dev.diego.dscatalog.entities.Category;
 import br.dev.diego.dscatalog.repositories.CategoryRepository;
+import br.dev.diego.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,9 +19,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findaAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
 	}
-	
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Id não encontrado!"));
+		return new CategoryDTO(entity);
+	}
 }
---
+++ backend/src/main/java/br/dev/diego/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package br.dev.diego.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
