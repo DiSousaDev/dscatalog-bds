e963d214e8ae5822e4cf2d6dd7a62dd9f844eab5
Diego Sousa <diegosous@gmail.com>
2020-10-11 01:01:56 -0300

    Finish product details

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,17 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './core/components/Navbar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
+import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 
 const Routes = () => (
     <BrowserRouter>
         <Navbar />
         <Switch>
-            <Route path="/" exact> 
+            <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/catalog">
+            <Route path="/products" exact>
                 <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
             </Route>
             <Route path="/admin">
                 <Admin />
