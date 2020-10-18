ea155036e81c97904ae8036d5fa019dd27eb6c75
Diego Sousa <diegosous@gmail.com>
2020-10-17 21:20:00 -0300

    Add admin NavBar

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import Pagination from 'core/components/Pagination';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
     const [productResponse, setProductResponse] = useState<ProductResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
+        setIsLoading(true);
         makeRequest({ url: '/products', params })
-            .then(response => setProductResponse(response.data));
-    }, []);
+            .then(response => setProductResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
             <h1 className="catalog-title">Catálogo de produtos</h1>
             <div className="catalog-products">
-                {productResponse?.content.map(product => (<Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product}/></Link>))}                
+                {isLoading ? <ProductCardLoader /> : (
+                    productResponse?.content.map(product => (<Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product} /></Link>))
+                )}
             </div>
+            {productResponse && (<Pagination totalPages={productResponse.totalPages} activePage={activePage} onChange={page => setActivePage(page)} />)}
         </div>
     );
 }
